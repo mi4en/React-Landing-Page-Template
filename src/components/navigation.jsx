577c014570d9b300d18026b61c51b56a646d692b
mi4en <marionikolov87@gmail.com>
2021-05-04 08:32:55 +0300

    update browser tab title; update navigation;

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = () => {
 						<span className='icon-bar'></span>{' '}
 					</button>
 					<a className='navbar-brand page-scroll' href='#page-top'>
-						React Landing Page
+						Na Achi Kremcheto
 					</a>{' '}
 				</div>
 

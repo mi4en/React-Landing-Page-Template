e2a422c09202d830098f99a452dc64adb6bf7a33
mi4en <marionikolov87@gmail.com>
2021-05-11 08:27:36 +0300

    ome text updates;

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,9 +9,9 @@ const About = props => {
 					</div>
 					<div className='col-xs-12 col-md-6'>
 						<div className='about-text'>
-							<h2>About Us</h2>
+							<h2>About Nashto Kremche</h2>
 							<p>{props.data ? props.data.paragraph : 'loading...'}</p>
-							<h3>Why Choose Us?</h3>
+							<h3>Why Choose Nashto Kremche?</h3>
 							<div className='list-style'>
 								<div className='col-lg-6 col-sm-6 col-xs-12'>
 									<ul>

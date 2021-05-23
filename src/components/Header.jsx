5ac9cc117517bd4ac42f60e297acf006fd5a965e
mi4en <marionikolov87@gmail.com>
2021-05-23 20:39:52 +0300

    update intro texts positioning;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,14 @@ const Header = props => {
 					<div className='container'>
 						<div className='row'>
 							<div className='col-md-8 col-md-offset-2 intro-text'>
-								<h1>{props.data ? props.data.title : 'Loading'}</h1>
-								<p>{props.data ? props.data.paragraph : 'Loading'}</p>
+								<h1>{props.data ? props.data.title1 : 'Loading'}</h1>
+								<h1 id='title2'>
+									{props.data ? props.data.title2 : 'Loading'}
+								</h1>
+								<p>{props.data ? props.data.paragraph1 : 'Loading'}</p>
+								<p id='paragraph2'>
+									{props.data ? props.data.paragraph2 : 'Loading'}
+								</p>
 								<a
 									href='#services'
 									className='btn btn-custom btn-lg page-scroll'
